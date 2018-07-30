f61ef995f79af19073e48d6e3408f7e275fb8182
Mohan <Mohan@wuhandeMac-Pro.local>
2018-07-30 23:23:02 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -28,11 +28,12 @@ export default {
     return {
       tag : 'li',
       nowUrl : '/home',
+      preIndex : 0,//记录路由的上一个index
       liData : [
-        { name : '首页' , url : '/home' , icon : 'icon-shouye'} ,
-        { name : '分类' , url : '/classify' , icon : 'icon-fenlei'},
-        { name : '购物车' , url : '/shoppingcar' , icon : 'icon-gouwuche'},
-        { name : '我的' , url : '/mine' , icon : 'icon-smile'}
+        { name : '首页' , url : '/home' , icon : 'icon-shouye' , index : 0} ,
+        { name : '分类' , url : '/classify' , icon : 'icon-fenlei' ,index : 1},
+        { name : '购物车' , url : '/shoppingcar' , icon : 'icon-gouwuche' , index : 2},
+        { name : '我的' , url : '/mine' , icon : 'icon-smile' ,index : 3}
       ],
       transitionType : 'slide-go'
     };
@@ -44,8 +45,16 @@ export default {
   },
   watch : {
     '$route'(to , from) {
-      console.log(to)
-      console.log(from)
+      this.liData.forEach((val) => {
+        if(val.url == to.path && !!from.path) {
+          if(val.index > this.preIndex) {
+            this.transitionType = 'slide-go';
+          } else if(val.index < this.preIndex){
+            this.transitionType = 'slide-back';
+          }
+          this.preIndex = val.index;
+        }
+      }) 
       this.nowUrl = to.fullPath;
     }
   },
