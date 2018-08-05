6938c7075bfebc253bff041bf3bc46e7942ba727
Mohan <Mohan@wuhandeMac-Pro.lan>
2018-08-05 23:20:39 +0800

    添加home页

--- src/router/index.js
+++ src/router/index.js
@@ -4,42 +4,48 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  mode : 'history',
-  routes: [
-    {
-      path: '/' ,
-      redirect : '/home'
-    },
-    {
-      path : '/home',
-      name : 'home',
-      // component : () => import('@/views/home.vue')
-      // component : (resolve) => require(['@/views/home.vue'] ,resolve)
-      // component(resolve){ require.ensure([] , () => resolve(require('@/views/home.vue')))}
-      component(resolve) {
-        require.ensure([] , () => resolve(require('@/views/home')))
-      }
-    },
-    {
-      path: '/classify',
-      name: 'classify',
-      component(resolve){
-        require.ensure([] , () => resolve(require('@/views/classify')))
-      }
-    },
-    {
-      path : '/shoppingcar',
-      name : 'shoppingcar',
-      component(resolve) {
-        require.ensure([] , () => resolve(require('@/views/shoppingcar')))
-      }
-    },
-    {
-      path : '/mine',
-      name : 'mine',
-      component(resolve) {
-        require.ensure([] , () => resolve(require('@/views/mine')))
-      }
-    }
-  ]
+    mode: 'history',
+    routes: [{
+            path: '/',
+            redirect: '/home'
+        },
+        {
+            path: '/home',
+            name: 'home',
+            // component : () => import('@/views/home.vue')
+            // component : (resolve) => require(['@/views/home.vue'] ,resolve)
+            // component(resolve){ require.ensure([] , () => resolve(require('@/views/home.vue')))}
+            component(resolve) {
+                require.ensure([], () => resolve(require('@/views/home')))
+            },
+            children: [{
+                path: 'a',
+                name: 'a',
+                component(resolve) {
+                    require.ensure([], () => resolve(require('@/views/classify')))
+                }
+            }]
+        },
+        {
+            path: '/classify',
+            name: 'classify',
+            component(resolve) {
+                require.ensure([], () => resolve(require('@/views/classify')))
+            }
+        },
+        {
+            path: '/shoppingcar',
+            name: 'shoppingcar',
+            component(resolve) {
+                require.ensure([], () => resolve(require('@/views/shoppingcar')))
+            }
+        },
+        {
+            path: '/mine',
+            name: 'mine',
+            component(resolve) {
+                require.ensure([], () => resolve(require('@/views/mine')))
+            }
+        }
+    ]
 })
